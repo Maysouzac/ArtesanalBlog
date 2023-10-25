deeee231accdfd7fee576a7b5c088357525d5b3c
Mayara <mayarasouzacosta@hotmail.com>
2023-10-24 22:05:57 -0300

    first commit

---
+++ src/main/java/com/artesanal/ArtesanalApplication.java
@@ -0,0 +1,13 @@
+package com.artesanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesanalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtesanalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/artesanal/model/Imagem.java
@@ -0,0 +1,24 @@
+package com.artesanal.model;
+
+public class Imagem {
+
+	 private String name;
+	 
+	    private byte[] data;
+	    
+	    // Getters and setters
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public byte[] getData() {
+			return data;
+		}
+		public void setData(byte[] data) {
+			this.data = data;
+		}
+
+	    
+}
